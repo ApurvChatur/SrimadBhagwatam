09ebb7d30386849abacaae510fbbffa0f9934cb0
ApurvChatur <chaturapurv@gmail.com>
2024-03-18 17:13:20 +0530

    Chor Police Again

--- site/frontend/vite.config.js
+++ site/frontend/vite.config.js
@@ -12,16 +12,16 @@ export default defineConfig({
         "name":"Srimad Bhagwatam",
         "short_name":"Srimad Bhagwatam",
         "display": "standalone",
-        "background_color": "#071952",
-        "theme_color": "#071952",
+        "background_color": "#990011",
+        "theme_color": "#990011",
         "icons":[
           {
-            src: "Business/ApurvChatur/favicon_io/android-chrome-512x512.png",
+            src: "Business/SrimadBhagwatam/favicon_io/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "Business/ApurvChatur/favicon_io/android-chrome-192x192.png",
+            src: "Business/SrimadBhagwatam/favicon_io/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
